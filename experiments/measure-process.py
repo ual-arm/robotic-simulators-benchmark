560d94228ac7feff660bb87336ed21b2fa297cce
Jose Luis Blanco-Claraco <joseluisblancoc@gmail.com>
2023-04-14 09:50:22 +0200

    measure script

--- experiments/measure-process.py
+++ experiments/measure-process.py
@@ -1,10 +1,24 @@
 #!/usr/bin/env python3
 
+# Usage example:
+#
+# ./measure-process.py --process mvsim
+#
+# ./measure-process.py --process gzserver --process gzclient   # sums both
+
 import psutil
 import time
 
+import argparse
 
-PROCESS_NAME = 'mvsim'
+parser = argparse.ArgumentParser()
+parser.add_argument("--process", type=str,
+                    help="name of the process to measure", required=True, action='append')
+parser.add_argument("--period", type=float,
+                    help="period in seconds to wait for the measure (<1 second is ok)", required=False, default=0.5)
+
+args = parser.parse_args()
+
 val = 0
 
 
@@ -13,15 +27,14 @@ def do_measure():
     pi = psutil.process_iter()
     lst = {}
     for proc in pi:
-        if proc.name() != PROCESS_NAME:
+        if not proc.name() in args.process:
             continue
 
         # We need to call this twice to get a first meaningful value
-        val = proc.cpu_percent()
-        break
+        val += proc.cpu_percent()
 
 
 do_measure()
-time.sleep(0.5)
+time.sleep(args.period)
 do_measure()
 print(val)
