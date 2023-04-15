dc50d4fa81be348a667fe140e9534842513e5aba
FranciscoJManasAlvarez <fma527@ual.es>
2023-04-16 00:28:11 +0200

    Update node

--- experiments/measure_process_ros2_pkg/measure_process_ros2_pkg/measure_process.py
+++ experiments/measure_process_ros2_pkg/measure_process_ros2_pkg/measure_process.py
@@ -1,22 +1,29 @@
 import rclpy
 from rclpy.node import Node
-from std_msgs.msg import String, Float64
+from std_msgs.msg import String, Float64, Float64MultiArray
 import psutil
-import time
+import numpy as np
 
+class Process():
+    def __init__(self, parent, id, topic):
+        self.id = id
+        self.value = 0.0
+        self.node = parent
+        self.node.get_logger().warn('Init %s' % (self.id))
+        self.pub_process_measure_ = self.node.create_publisher(Float64, topic.replace("-","_"), 10)
 
 class MeasureProcess(Node):
     def __init__(self):
         super().__init__('measure_process')
         # Params
-        self.declare_parameter('process_name', 'webots-bin')
+        self.declare_parameter('process_name', 'gzserver')
         self.declare_parameter('process_period', 0.5)
 
         # Subscription
         self.status_ = self.create_subscription(String, 'status', self.status_callback, 10)
         
         # Publisher
-        self.pub_process_measure_ = self.create_publisher(Float64, 'process_value', 10)
+        self.pub_cpu_measure_ = self.create_publisher(Float64MultiArray, 'cpu_stats', 10)
 
         # Variables
         self.value = 0.0
@@ -28,6 +35,12 @@ class MeasureProcess(Node):
         self.get_logger().info('Measure Process::inicialize() ok.')
         # Read Params
         self.process_name = self.get_parameter('process_name').get_parameter_value().string_value
+        self.proc_list = self.process_name.split(', ')
+        print(self.proc_list)
+        self.process_list = list()
+        for process in self.proc_list:
+            proc = Process(self, process, process+'_cpu')
+            self.process_list.append(proc)
         process_period = self.get_parameter('process_period').get_parameter_value().double_value
 
         self.timer = self.create_timer(process_period, self.iterate)
@@ -36,25 +49,42 @@ class MeasureProcess(Node):
         self.get_logger().info('Status: "%s"' % msg.data)
 
     def do_measure(self):
+        data_list = list()
+        cpu_count = psutil.cpu_count()
+        data_list.append(float(cpu_count))
+        data_list.append(psutil.cpu_percent(interval=None))
+        mem = psutil.virtual_memory()
+        data_list.append(mem.percent)
+        load_tuple = psutil.getloadavg()
+        for load in load_tuple:
+            percent = (load / cpu_count) * 100
+            data_list.append(percent)
+
+        msg = Float64MultiArray()
+        msg.data = data_list
+        self.pub_cpu_measure_.publish(msg)
         pi = psutil.process_iter()
-        lst = {}
-        
+
         for proc in pi:
-            if not proc.name() in self.process_name:
+            if not proc.name() in self.proc_list:
                 continue
 
-            # We need to call this twice to get a first meaningful value
             value =  proc.cpu_percent()
             if not value == 0.0:
-                self.value = value
-                self.get_logger().debug('Measure Process :: Value = %f' % self.value)
+                for process in self.process_list:
+                    if proc.name() == process.id:
+                        process.value += value
+        
+        msg = Float64()
+        for process in self.process_list: 
+            msg.data = process.value
+            process.pub_process_measure_.publish(msg)
+            self.get_logger().debug('Process %s = %f' % (process.id, msg.data))
+            process.value = 0.0
+
 
     def iterate(self):
         self.do_measure()
-        msg = Float64()
-        msg.data = self.value
-        self.pub_process_measure_.publish(msg)
-        
 
 def main(args=None):
     rclpy.init(args=args)
