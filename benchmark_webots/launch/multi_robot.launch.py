464bfd430a0694f7eb76e032c309d5e8a4bc3634
FranciscoJManasAlvarez <fma527@ual.es>
2023-06-09 15:08:54 +0200

    Webots sh file

--- benchmark_webots/launch/multi_robot.launch.py
+++ benchmark_webots/launch/multi_robot.launch.py
@@ -8,11 +8,15 @@ from launch.substitutions import LaunchConfiguration
 from webots_ros2_driver.webots_launcher import WebotsLauncher, Ros2SupervisorLauncher
 from webots_ros2_driver.utils import controller_url_prefix
 
+def str2bool(v):
+  return v.lower() in ("yes", "true", "t", "1")
 
 def generate_launch_description():
     package_dir = get_package_share_directory('benchmark_webots')
     robot_description = pathlib.Path(os.path.join(package_dir, 'resource', 'robot.urdf')).read_text()
     use_sim_time = LaunchConfiguration('use_sim_time', default=True)
+
+    show_gui = str2bool(os.getenv('BENCHMARK_GUI', 'False'))
 
     # Number of robots
     num_robots = int(os.getenv('BENCHMARK_NUM_ROBOTS', 1))
@@ -47,8 +51,23 @@ def generate_launch_description():
         name='benchmark',
         output='screen',
         parameters=[{
-             'process_name' : 'webots-bin, driver, ros2, rviz2',
-             'process_period' : 0.1},
+             'process_name' : 'webots-bin',
+             'process_period' : 1.0},
+        ],
+    )
+
+    stats_recorder_webots = Node(
+        package='measure_process_ros2_pkg',
+        executable='record_cpu_usage',
+        name='stats_recorder_webots',
+        output='screen',
+        on_exit=launch.actions.Shutdown(),
+        parameters=[{
+             'output_file' : '/tmp/cpu_webots_gui_{}_{:02d}.txt'.format(show_gui, num_robots),
+             'initial_delay' : 10,
+             'number_samples' : 60,
+             'topic_array_index' : 0  # Index within "process_name[]" of measure_process
+             },
         ],
     )
 
@@ -63,6 +82,7 @@ def generate_launch_description():
     ld = LaunchDescription()
     ld.add_action(webots)
     ld.add_action(cpu_measure)
+    ld.add_action(stats_recorder_webots)
     for robot in robot_node_list:
         ld.add_action(robot)
         
---
+++ run_benchmark1_webots.sh
@@ -0,0 +1,20 @@
+#!/bin/bash
+
+# Usage: Build and "source install/setup.bash" this repo with ROS2 & colcon first.
+#        Then invoke this script.
+
+rm /tmp/cpu*
+
+for WITH_GUI in False True; do
+    for N in {1..25..2}; do
+    clear
+    echo "========================================================"
+    echo " LAUNCHING BENCHMARK FOR $N ROBOTS  (GUI: True)    "
+    echo "========================================================"
+
+    BENCHMARK_NUM_ROBOTS=$N ros2 launch benchmark_webots multi_robot.launch.py
+    done
+done
+
+mkdir -p ./results/benchmark_n_robots
+mv -v /tmp/cpu*.txt ./results/benchmark_n_robots
