b6a1645f0291ddf4952d6c3bbe0351dd4abda88d
FranciscoJManasAlvarez <fma527@ual.es>
2023-06-10 16:42:02 +0200

    ROS2Supervisor controller fixed

--- benchmark_webots/launch/multi_robot.launch.py
+++ benchmark_webots/launch/multi_robot.launch.py
@@ -5,26 +5,24 @@ from launch_ros.actions import Node
 from launch import LaunchDescription
 from ament_index_python.packages import get_package_share_directory
 from launch.substitutions import LaunchConfiguration
-from webots_ros2_driver.webots_launcher import WebotsLauncher, Ros2SupervisorLauncher
+from launch.actions import DeclareLaunchArgument
+from launch.substitutions.path_join_substitution import PathJoinSubstitution
+from launch import LaunchDescription
+from webots_ros2_driver.webots_launcher import WebotsLauncher
+from webots_ros2_driver.wait_for_controller_connection import WaitForControllerConnection
 from webots_ros2_driver.utils import controller_url_prefix
 
 def str2bool(v):
   return v.lower() in ("yes", "true", "t", "1")
 
-def generate_launch_description():
+def get_ros2_nodes(*args):
     package_dir = get_package_share_directory('benchmark_webots')
     robot_description = pathlib.Path(os.path.join(package_dir, 'resource', 'robot.urdf')).read_text()
     use_sim_time = LaunchConfiguration('use_sim_time', default=True)
-    ros2_supervisor = Ros2SupervisorLauncher()
-
     show_gui = str2bool(os.getenv('BENCHMARK_GUI', 'False'))
 
     # Number of robots
     num_robots = int(os.getenv('BENCHMARK_NUM_ROBOTS', 1))
-
-    webots = WebotsLauncher(
-        world=os.path.join(package_dir, 'worlds', 'N'+str(num_robots)+'_world.wbt')
-    )
 
     robot_node_list = []
 
@@ -45,48 +43,83 @@ def generate_launch_description():
                                 )
         )
 
-    cpu_measure = Node(
-        package='measure_process_ros2_pkg',
-        executable='measure_process',
-        name='benchmark',
-        output='screen',
-        parameters=[{
-             'process_name' : 'webots-bin',
-             'process_period' : 1.0},
-        ],
+    robot_node_list.append(Node(
+            package='measure_process_ros2_pkg',
+            executable='measure_process',
+            name='benchmark',
+            output='screen',
+            parameters=[{
+                'process_name' : 'webots-bin',
+                'process_period' : 1.0},
+            ],
+        )
     )
 
-    stats_recorder_webots = Node(
-        package='measure_process_ros2_pkg',
-        executable='record_cpu_usage',
-        name='stats_recorder_webots',
-        output='screen',
-        on_exit=launch.actions.Shutdown(),
-        parameters=[{
-             'output_file' : '/tmp/cpu_webots_gui_{}_{:02d}.txt'.format(show_gui, num_robots),
-             'initial_delay' : 10,
-             'number_samples' : 60,
-             'topic_array_index' : 0  # Index within "process_name[]" of measure_process
-             },
-        ],
+    robot_node_list.append(Node(
+            package='measure_process_ros2_pkg',
+            executable='record_cpu_usage',
+            name='stats_recorder_webots',
+            output='screen',
+            on_exit=launch.actions.Shutdown(),
+            parameters=[{
+                'output_file' : '/tmp/cpu_webots_gui_{}_{:02d}.txt'.format(show_gui, num_robots),
+                'initial_delay' : 10,
+                'number_samples' : 60,
+                'topic_array_index' : 0  # Index within "process_name[]" of measure_process
+                },
+            ],
+        )
     )
 
-    ros2_close = launch.actions.RegisterEventHandler(
-                    event_handler=launch.event_handlers.OnProcessExit(
-                    target_action=webots,
-                    on_exit=[launch.actions.EmitEvent(event=launch.events.Shutdown())],
-                    )
-                )
+    return robot_node_list
 
+def generate_launch_description():
+    package_dir = get_package_share_directory('benchmark_webots')
+    world = LaunchConfiguration('world')
+    mode = LaunchConfiguration('mode')
+    webots = WebotsLauncher(
+        world=PathJoinSubstitution([package_dir, 'worlds', world]),
+        mode=mode,
+        ros2_supervisor=True
+    )
 
-    ld = LaunchDescription()
-    ld.add_action(webots)
-    ld.add_action(ros2_supervisor)
-    # ld.add_action(cpu_measure)
-    # ld.add_action(stats_recorder_webots)
-    for robot in robot_node_list:
-        ld.add_action(robot)
-        
-    ld.add_action(ros2_close)
+    reset_handler = launch.actions.RegisterEventHandler(
+        event_handler=launch.event_handlers.OnProcessExit(
+            target_action=webots._supervisor,
+            on_exit=get_ros2_nodes,
+        )
+    )
 
-    return ld
+    num_robots = int(os.getenv('BENCHMARK_NUM_ROBOTS', 1))
+
+    return LaunchDescription([
+        DeclareLaunchArgument(
+            'world',
+            default_value='N'+str(num_robots)+'_world.wbt',
+            description='Benchmark World'
+        ),
+        DeclareLaunchArgument(
+            'mode',
+            default_value='realtime',
+            description='Webots startup mode'
+        ),
+        webots,
+        webots._supervisor,
+
+        # This action will kill all nodes once the Webots simulation has exited
+        launch.actions.RegisterEventHandler(
+            event_handler=launch.event_handlers.OnProcessExit(
+                target_action=webots,
+                on_exit=[
+                    launch.actions.UnregisterEventHandler(
+                        event_handler=reset_handler.event_handler
+                    ),
+                    launch.actions.EmitEvent(event=launch.events.Shutdown())
+                ],
+            )
+        ),
+
+        # Add the reset event handler
+        reset_handler
+    ] + get_ros2_nodes())
+
