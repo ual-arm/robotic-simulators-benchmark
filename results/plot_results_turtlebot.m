f524d646eff5c0d7a6e98adf1bca529891712532
Jose Luis Blanco-Claraco <joseluisblancoc@gmail.com>
2023-06-11 10:19:45 +0200

    WIP

--- results/plot_results_turtlebot.m
+++ results/plot_results_turtlebot.m
@@ -1,6 +1,7 @@
 function []=plot_results_turtlebot()
+close all;
 
-data = cell(3);  % {1,2}: gzserver, gazebo, mvsim
+data = cell(5);  % {1,2}: gzserver, gazebo, mvsim
 
 data{1} = load('benchmark_turtlebot/cpu_gzserver.txt');
 data{2} = load('benchmark_turtlebot/cpu_mvsim_no_gui.txt');
@@ -8,20 +9,19 @@ data{2} = load('benchmark_turtlebot/cpu_mvsim_no_gui.txt');
 data{3} = load('benchmark_turtlebot/cpu_gzserver.txt') + ...
           load('benchmark_turtlebot/cpu_gzclient.txt');
 
-data{4} = load('benchmark_turtlebot/cpu_mvsim.txt');
+data{4} = load('benchmark_turtlebot/cpu_webots_bin.txt')+...
+          load('benchmark_turtlebot/cpu_webots_driver.txt')+...
+          load('benchmark_turtlebot/cpu_webots_ros2.txt');
 
-numSamples = length(data{1});
+data{5} = load('benchmark_turtlebot/cpu_mvsim.txt');
 
-% Calculate statistics for each set
-%means = cellfun(@mean, data);
-%stds = cellfun(@std, data);
 
 % Plotting the figure
 
 sep=0.35; % separation between simulators points
 ptSize = 6.0;
 
-for i=1:4
+for i=1:5
     upperBound(i) = prctile(data{i},95);
     lowerBound(i) = prctile(data{i},5);
     median(i) = prctile(data{i},50);
@@ -44,21 +44,16 @@ end
 afigure;
 hold on;
 
-% Example data
-data1 = [median(1) median(2)];
-data2 = [median(3) median(4)];
+% data
+data = [median(1) 0 median(2); ...
+        median(3) median(4) median(5)];
 
 % Create a vector for the positions of the bars
 positions = 1:2;
 
-% Create a vector for the bar heights
-heights1 = [data2(2) 0; data2(1) 0];
-heights2 = [0 data1(2); 0 data1(1)];
-
 % Plot the bar graph
-barh(positions, heights1, 'b');
+barh(positions, data);
 hold on;
-barh(positions, heights2, 'r');
 
 plot_segment(positions(1)-0.15,lowerBound(1),upperBound(1));
 plot_segment(positions(1)+0.15,lowerBound(2),upperBound(2));
@@ -71,7 +66,7 @@ yticks(positions);
 yticklabels({'Headless', 'With GUI'});
 
 % Set the legend
-legend('Gazebo','', 'MVSim');
+legend('Gazebo','Webots', 'MVSim');
 
 % Set the axis labels and title
 xlabel('CPU core usage [%]');
